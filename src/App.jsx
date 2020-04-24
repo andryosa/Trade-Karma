3b031e4a320b0f94e7faa249a7fa7941e3a60ce1
Andryosa <andryos44@gmail.com>
2020-04-24 13:24:52 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
-import Navbar from './component/Navbar/Navbar'
-import Body from './component/Body/Body'
-import VideoSection from './component/VideoSection'
+import Navbar from './component/Navbar/Navbar';
+import Body from './component/Body/Body';
+import VideoSection from './component/VideoSection';
+import About from './component/About/About';
+import Contact from './component/Contact/contact';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar/>
-      <Body id='home'/>
-      <VideoSection id='about'/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/contact' component={Contact}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <Body/>
+    <VideoSection/>
+  </div>
+)
 
 export default App;
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import "./Navbar.css";
 import Logogramcolor from "../logo/logogram-white.png";
+import {Link} from 'react-router-dom';
 
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="nav">
-        <a href="#"><img src={Logogramcolor} alt=""/></a>
-        <a href="#">Home</a>
+      <nav className="nav">
+        <ul className='nav-links'>
+          <Link to='/'>
+              <li><img src={Logogramcolor} alt="" className='logo'/></li>
+          </Link>
+          <Link className='links'  to='/'>
+              <li>Home</li>
+          </Link>
+          <Link className='links' to='/about'>
+              <li>About Us</li>
+          </Link>
+          <Link className='links' to='/contact'>
+              <li>Contact</li>
+          </Link>
+        </ul>
+        {/* <a href="#">Home</a>
         <a href="#">About Us</a>
-        <a href="#">Contact</a>
-      </div>
+        <a href="#">Contact</a> */}
+      </nav>
     );
   }
 }
